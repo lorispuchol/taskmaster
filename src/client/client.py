58168131a784dc57898523514b1efede1e0648c7
Loris Puchol <lorispuchol@gmail.com>
2024-11-18 09:40:09 -0500

    client partially good (lake of loop message entering in client

--- src/client/client.py
+++ src/client/client.py
@@ -1,43 +1,98 @@
 import socket
+import selectors
+import types
 import sys
 
+sel = selectors.DefaultSelector()
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
-
-
-def start_client(args):
-
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((HOST, PORT))  # Attempt to connect to the server
-            print(f"Connected to server at {HOST}:{PORT}")
-            while True:
-                message = input("Enter message to send (or 'quit' to exit): ")
-                if not message:
-                    continue
-                if message.lower() == "quit":
-                    break
-                s.sendall(message.encode())
-                data = s.recv(1024)
-                print(f"Received from server: {data.decode()}")
-                if (data.decode() == "close you"):
-                    s.shutdown(socket.SHUT_RDWR)
-                    s.close()
-                    print("Connection closed")
-    except socket.gaierror as e:
-        print(f"Address-related error connecting to server: {e}")
-    except socket.timeout as e:
-        print(f"Connection timed out: {e}")
-    except socket.error as e:
-        print(f"Socket error: {e}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-    except KeyboardInterrupt:
-        print("\nExiting...")
-    finally:
-        s.close()
+messages = [b"Message 1 from client.", b"Message 2 from client."]
 
 
-if __name__ == "__main__":
-    start_client(sys.argv)
+# 'num_conns' is read from the command-line, which is the number of connections to create to the server
+def start_connections(host, port):
+    server_addr = (host, port)
+    print("starting connection to", server_addr)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # each socket is set to non-blocking mode
+    sock.setblocking(False)
+
+    # 'connect_ex()' is used instead of 'connect()' since 'connect()' would immediately raise a
+    # 'BlockingIOError' exception.
+    # 'connect_ex()' initially returns an erro indicator 'errno.EINPROGRESS', instead of raising
+    # an exception while the connection is in progress
+    sock.connect_ex(server_addr)
+
+    # once the connection is completed, the socket is ready for reading and writing
+    # the status is returned by 'select()'
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+
+    # creates data we want stored in the socket
+    # messages the client will send to the server are copied using 'list(messages)' since each
+    # connection will call 'socket.sent()' and modify the list.
+    # connid - connection id
+    # msg_total - total bytes of messages sent
+    # recv_total - total bytes of messages received
+    # messages - message contents sent
+    # outb - message that is sent per 'send()' operation
+    data = types.SimpleNamespace(
+        msg_total=sum(len(m) for m in messages),
+        recv_total=0,
+        messages=list(messages),
+        outb=b"",
+    )
+    sel.register(sock, events, data=data)
+
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # should be ready to read
+        if recv_data:
+            print("received", repr(recv_data), "from server")
+
+            # keeps track of the number of bytes received from the server
+            data.recv_total += len(recv_data)
+
+        # when it is not receiving any data or when 'data.recv_total' == 'data.msg_total', close connection
+        if not recv_data or data.recv_total == data.msg_total:
+            print("closing connection")
+            sel.unregister(sock)
+            sock.shutdown(socket.SHUT_WR)
+            sock.close()
+
+    if mask & selectors.EVENT_WRITE:
+        if not data.outb and data.messages:
+            # takes out the last element in the list and saves it to 'data.outb'
+            data.outb = data.messages.pop(0)
+        if data.outb:
+            print("sending", repr(data.outb), "to connection")
+
+            # sends 'data.outb'
+            sent = sock.send(data.outb)  # should be ready to write
+
+            # empties 'data.outb'
+            data.outb = data.outb[sent:]
+
+
+if len(sys.argv) != 3:
+    print("usage:", sys.argv[0], "<host> <port>")
+    sys.exit(1)
+
+
+start_connections(sys.argv[1], int(sys.argv[2]))
+
+try:
+    while True:
+        events = sel.select(timeout=1)
+        if events:
+            for key, mask in events:
+                service_connection(key, mask)
+            # Check for a socket being monitored to continue
+            if not sel.get_map():
+                break
+except KeyboardInterrupt:
+    print("caught keyboard interrupt, exiting")
+finally:
+    sel.close()
